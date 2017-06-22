d6c3c965c5d2d1eeb9b3415793fb7e4afb516e05
Ben Pluim <benpluim@Bens-MacBook-Pro.local>
2017-06-21 20:49:39 -0600

    API practice

--- js/itunesService.js
+++ js/itunesService.js
@@ -2,12 +2,49 @@ angular.module('itunes').service('itunesService', function($http, $q){
   //This service is what will do the 'heavy lifting' and get our data from the iTunes API.
   //Also note that we're using a 'service' and not a 'factory' so all your methods you want to call in your controller need to be on 'this'.
 
-  //Write a method that accepts an artist's name as the parameter, then makes a 'JSONP' http request to a url that looks like this
+  //Write a method that 
+  //accepts an artist's name as the parameter, 
+  //then makes a 'JSONP' http request to a url that looks like this
   //https://itunes.apple.com/search?term=' + artist + '&callback=JSON_CALLBACK'
   //Note that in the above line, artist is the parameter being passed in. 
   //You can return the http request or you can make your own promise in order to manipulate the data before you resolve it.
 
     //Code here
+
+    this.getArtist = function(artist) {
+      var deferred = $q.defer();
+      $http({
+        method: 'JSONP',
+        url: 'https://itunes.apple.com/search?term=' + artist + '&callback=JSON_CALLBACK'
+      })
+      .then(function(response) {
+        var results = response.data.results;
+        console.log(results);
+        var newResults = [];
+        function newResultsFormat(artworkUrl100, artistName, trackCensoredName, collectionName, collectionPrice, previewUrl, kind) {
+          this.AlbumArt = artworkUrl100;
+          this.Artist = artistName;
+          this.Song = trackCensoredName;
+          this.Collection = collectionName;
+          this.CollectionPrice = collectionPrice;
+          this.Play = previewUrl;
+          this.Type = kind;
+        }
+
+        for (var i = 0; i < results.length; i++) {
+
+          var newRes = new newResultsFormat(results[i].artworkUrl100, results[i].artistName, results[i].trackCensoredName, results[i].collectionName, results[i].collectionPrice, results[i].previewUrl, results[i].kind)
+          
+          newResults.push(newRes);
+          
+        }
+        // console.log(newResults);
+        deferred.resolve(newResults);
+      });
+
+      return deferred.promise;
+    };
+
     
 
 
